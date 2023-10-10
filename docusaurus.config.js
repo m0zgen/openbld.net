e9c6e9fb2f954c16310ddeb5fae5a0f9d07ca944
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-10-10 19:16:54 +0600

    ~ changed edit links url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,9 +20,9 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'm0zgen', // Usually your GitHub org/user name.
   projectName: 'openbld.net', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  // deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
@@ -43,14 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/m0zgen/openbld.net/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
