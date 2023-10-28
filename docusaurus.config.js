2dd1031ea33b15d3bb2788af6d3d7b0d5839eecf
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-10-28 07:56:52 +0600

    + ~ OpenBLD Social Card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/open-bld-og-logo-1200.jpg',
       navbar: {
         title: 'OpenBLD.net DNS',
         logo: {
