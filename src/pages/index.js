fbd33e9908084027b4efa879485f8a51b806b176
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-03-31 00:08:30 +0500

    + + Sponsorship to Benefits link

--- src/pages/index.js
+++ src/pages/index.js
@@ -125,7 +125,11 @@ function FeaturesListHeading() {
                                             <svg class="w-3.5 h-3.5 mr-2 mb-3 text-red-500 dark:text-red-500 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                                 <path d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" fill-rule="evenodd"></path>
                                             </svg>
-                                            <p><a className="text-blue-600 dark:text-blue-500" href="/docs/overwiew/openbld-plus">Get benefits from Donations</a></p>
+                                            <p>
+                                                <a className="text-blue-600 dark:text-blue-500" href="/docs/overwiew/openbld-plus">
+                                                    Get benefits from Donations / Sponsorship
+                                                </a>
+                                            </p>
                                         </li>
                                     </ul>
                                 </div>
