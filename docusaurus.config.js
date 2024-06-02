b1c097e1e2b1020a3723c9a3a747fa95cd33345b
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-06-02 13:04:33 +0500

    + + Optionally docs collapsed main categories

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          // sidebarCollapsible: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
