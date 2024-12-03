6d7aefdaf1168d9322809e3af1e7bae523f81f6f
m0zgen <g.yevgeniy.p@gmail.com>
2024-12-03 05:14:42 +0000

    Deploying to gh-pages from @ m0zgen/openbld.net@294d6b316113c6574716cc212162691e331a3540 🚀

---
+++ ru/assets/js/30598abe.e102e62e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkopenbld_net=self.webpackChunkopenbld_net||[]).push([[6110],{83397:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/ru/blog/page/5","page":5,"postsPerPage":6,"totalPages":10,"totalCount":57,"previousPage":"/ru/blog/page/4","nextPage":"/ru/blog/page/6","blogDescription":"OpenBLD.net news and updates","blogTitle":"OpenBLD.net Blog"}}')}}]);
