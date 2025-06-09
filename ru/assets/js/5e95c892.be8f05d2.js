df0efb25c36bd51d449300d98cf89426695f9456
m0zgen <g.yevgeniy.p@gmail.com>
2025-06-09 11:34:31 +0000

    Deploying to gh-pages from @ m0zgen/openbld.net@3bb0cec92ab9eef0cbc2ebfe462d033a0280e73d 🚀

--- ru/assets/js/5e95c892.be8f05d2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkopenbld_net=self.webpackChunkopenbld_net||[]).push([[9647],{18122:(e,s,n)=>{n.r(s),n.d(s,{default:()=>d});n(96540);var r=n(34164),t=n(81082),u=n(204),a=n(22831),c=n(51410),l=n(74848);function d(e){return(0,l.jsx)(t.e3,{className:(0,r.A)(u.G.wrapper.docsPages),children:(0,l.jsx)(c.A,{children:(0,a.v)(e.route.routes)})})}}}]);
