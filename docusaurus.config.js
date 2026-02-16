ce82a1a974f84cd666b49bd274d041db44e8b06a
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2026-02-16 12:54:07 +0500

    + ~ Fix date (year) in copyright section in the footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -216,7 +216,7 @@ const config = {
             ],
           },
         ],
-        copyright: `© 2019-${new Date().getFullYear()} OpenBLD.net, Built with Docusaurus.`,
+        copyright: `© 2019-${new Date().getFullYear()} OpenBLD.net.`,
       },
       prism: {
         theme: lightTheme,
