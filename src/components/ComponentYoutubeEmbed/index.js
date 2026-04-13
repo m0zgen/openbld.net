1fa0e5f712fc35af9b33a4605db4387147fb0fa1
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2026-04-13 10:22:36 +0500

    + + Remove PropTypes

--- src/components/ComponentYoutubeEmbed/index.js
+++ src/components/ComponentYoutubeEmbed/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const YoutubeEmbed = ({ embedId }) => (
     <div className="video-responsive">
@@ -14,9 +13,5 @@ const YoutubeEmbed = ({ embedId }) => (
         />
     </div>
 );
-
-YoutubeEmbed.propTypes = {
-    embedId: PropTypes.string.isRequired
-};
 
 export default YoutubeEmbed;
